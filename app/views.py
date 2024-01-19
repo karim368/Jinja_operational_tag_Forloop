4393caeb676e348372838a5ce1d0e357d07e26d9
karim368 <karimever251@gmail.com>
2024-01-20 00:20:45 +0530

    first commit

--- app/views.py
+++ app/views.py
@@ -3,5 +3,5 @@ from django.shortcuts import render
 # Create your views here.
 
 def forloop(request):
-    d = {'name':'Karim'}
+    d = {'name':'Karim','numbers':(1,2,3,4,5,6,7,8,9,10)}
     return render(request,'forloop.html',d)
